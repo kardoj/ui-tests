5b1b36362c0b97e57b3999453a6255e20291437e
Kardo <kardoj@gmail.com>
2017-03-28 16:22:38 +0300

    Updates action-network-listener to check for player as well in addition to the recorder, adds a few console logs which actually will need a logger

--- scripts/renderer/action-network-listener.js
+++ scripts/renderer/action-network-listener.js
@@ -11,12 +11,14 @@ $(document).ready(() => {
 
 	$(document).on('performed-an-action', () => {
 		waitingForLoadingAfterAction = true;
+		console.log('waiting for loading to start after action');
 
 		// Cancel waiting after set time if loading has not started
 		setTimeout(() => {
 			if (isLoadingAfterAction) return;
 
 			waitingForLoadingAfterAction = false;
+			console.log('did not start loading after action');
 		}, Config.actionLoadingTimeout);
 	});
 
@@ -24,20 +26,22 @@ $(document).ready(() => {
 	// whether the page started loading as a result. If it did, it waits for the loading to complete
 	// and adds an URL check action
 	testSite.get(0).addEventListener('did-start-loading', () => {
-		if (!Recorder.isRecording()) return;
+		if (!Recorder.isRecording() && !Player.isPlaying()) return;
 		if (!waitingForLoadingAfterAction) return;
 
 		waitingForLoadingAfterAction = false;
 		isLoadingAfterAction = true;
+		console.log('started loading after action');
 	});
 
 	// If the recording was waiting for the loading to finish and the loading finished,
 	// add a navigation check event
 	testSite.get(0).addEventListener('did-stop-loading', () => {
-		if (!Recorder.isRecording()) return;
+		if (!Recorder.isRecording() && !Player.isPlaying()) return;
 		if (!isLoadingAfterAction) return;
 
 		$(document).trigger('finished-loading-after-performing-an-action');
 		isLoadingAfterAction = false;
+		console.log('finished loading after action');
 	});
 });
