e1925b0f968aaa82b88d73cb78b4c4aec5219b08
Kardo <kardoj@gmail.com>
2017-04-12 23:52:37 +0300

    Converts whitespace to tabs

--- scripts/renderer/test-site-listeners.js
+++ scripts/renderer/test-site-listeners.js
@@ -57,27 +57,27 @@ window.onload = () => {
 		}
 	}
 
-    // http://stackoverflow.com/questions/11661187/form-serialize-javascript-no-framework#answer-30153391
-    function serialize(form) {
-        var field, s = [];
-        if (typeof form == 'object' && form.nodeName == "FORM") {
-            var len = form.elements.length;
-            for (i=0; i<len; i++) {
-                field = form.elements[i];
-                if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
-                    if (field.type == 'select-multiple') {
-                        for (j=form.elements[i].options.length-1; j>=0; j--) {
-                            if(field.options[j].selected)
-                                s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[j].value);
-                        }
-                    } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
-                        s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value);
-                    }
-                }
-            }
-        }
-        return s.join('&').replace(/%20/g, '+');
-    }
+	// http://stackoverflow.com/questions/11661187/form-serialize-javascript-no-framework#answer-30153391
+	function serialize(form) {
+		var field, s = [];
+		if (typeof form == 'object' && form.nodeName == "FORM") {
+			var len = form.elements.length;
+			for (i=0; i<len; i++) {
+				field = form.elements[i];
+				if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
+					if (field.type == 'select-multiple') {
+						for (j=form.elements[i].options.length-1; j>=0; j--) {
+							if(field.options[j].selected)
+								s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[j].value);
+						}
+					} else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+						s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value);
+					}
+				}
+			}
+		}
+		return s.join('&').replace(/%20/g, '+');
+	}
 
 	function handleInput() {
 		if (inputElement !== null) {
