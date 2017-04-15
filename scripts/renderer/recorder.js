87c08bc096b286cd90fd5cbc8af591e65868c969
Kardo <kardoj@gmail.com>
2017-04-15 18:29:41 +0300

    Moves state logic from recorder to test site

--- scripts/renderer/recorder.js
+++ scripts/renderer/recorder.js
@@ -1,14 +1,5 @@
 let Recorder = {};
 ((ns, $) => {
-	// The recording can be in one of two different states:
-	// STATE_RECORD - is listening to all the events that are not related to adding a new check
-	// STATE_ADD_CHECK - is listening to only events which are related to adding a new check
-	// The reasoning behind two different states is that the recording can not be allowed
-	// to register the regular actions when adding a check (ClickAction for example, since it
-	// is used to select an element rather than register a click on it).
-	const STATE_RECORD = 'STATE_RECORD';
-	const STATE_ADD_CHECK = 'STATE_ADD_CHECK';
-
 	let isRecording = false;
 	let recording = null;
 	let startTime = null;
@@ -18,19 +9,6 @@ let Recorder = {};
 	let startRecordingBtn = null;
 	let stopRecordingBtn = null;
 	let testSite = null;
-
-	// State can be on record at all times, even if the test is not running
-	let state = STATE_RECORD;
-
-	// Used to keep track which actions to allow through in which state
-	let allowedActions = {
-		STATE_RECORD: ['click-event', 'input-event', 'nav-event', 'scroll-event'],
-		STATE_ADD_CHECK: ['click-event', 'scroll-event']
-	};
-
-	// Convenience method for more clearly checking if an action is allowed to be recorded
-	// channel is event name actually. Variable name channel is used to be consistent with ipc naming
-	function channelIsAllowedForState(channel, state) { return allowedActions[state].indexOf(channel) >= 0; }
 
 	// Asking isRecording from outside
 	ns.isRecording = function() { return isRecording; };
@@ -58,8 +36,7 @@ let Recorder = {};
 			testSite.get(0).openDevTools();
 		});
 
-		// Setting the appropriate state so adding a new check can happen
-		addAssertionBtn.on('add-assertion', () => { state = STATE_ADD_CHECK; });
+		addAssertionBtn.on('add-assertion', () => { testSite.get(0).send('add-check-state'); });
 
 		// testSite's preloaded script test-site-listeners.js has the listeners for these events.
 		// When interacting with the testSite, it captures the events and sends them
@@ -68,21 +45,16 @@ let Recorder = {};
 			if (!isRecording) return;
 			let actionData = e.args[0];
 
-			if (e.channel == 'scroll-event' && channelIsAllowedForState(e.channel, state)) {
+			if (e.channel == 'scroll-event') {
 				recording.addAction(new ScrollAction(actionData.x, actionData.y));
 				$(document).trigger('performed-an-action');
-			} else if (e.channel == 'click-event' && channelIsAllowedForState(e.channel, state)) {
-				if (state == STATE_RECORD) {
-					recording.addAction(new ClickAction(actionData.x, actionData.y, actionData.element.tagName));
-					testSite.get(0).send('click-forward', { x: actionData.x, y: actionData.y });
-					$(document).trigger('performed-an-action');
-				} else if (state == STATE_ADD_CHECK) {
-					console.log(actionData.element);
-				}
-			} else if (e.channel == 'input-event' && channelIsAllowedForState(e.channel, state)) {
+			} else if (e.channel == 'click-event') {
+				recording.addAction(new ClickAction(actionData.x, actionData.y, actionData.tagName));
+				$(document).trigger('performed-an-action');
+			} else if (e.channel == 'input-event') {
 				recording.addAction(new InputAction(actionData.x, actionData.y, actionData.input));
 				$(document).trigger('performed-an-action');
-			} else if (e.channel == 'nav-event' && channelIsAllowedForState(e.channel, state)) {
+			} else if (e.channel == 'nav-event') {
 				recording.addAction(new NavAction(actionData.url));
 				$(document).trigger('performed-an-action');
 			}
--- scripts/renderer/test-site-listeners.js
+++ scripts/renderer/test-site-listeners.js
@@ -10,27 +10,57 @@ let allowedInputTagNames = ['TEXTAREA', 'INPUT'];
 // Used to keep track of the input element while the user is typing
 let inputElement = null;
 
-let unfinishedClick = false;
+// Keeps track of the test site state
+// NB! All event emissions should go trough this object to make sure that state check is applied in one place
+let stateFilter = {
+	// The tested site can be in one of two different states:
+	// STATE_RECORD - is sending out all the events that are not related to adding a check
+	// STATE_ADD_CHECK - is sending out only the events that are related to adding a check
+	// The reasoning behind two different states is that the recording can not be allowed
+	// to register the regular actions when adding a check (ClickAction for example, since it
+	// is used to select an element rather than register a click on it).
+	states: {
+		STATE_RECORD: 'STATE_RECORD',
+		STATE_ADD_CHECK: 'STATE_ADD_CHECK'
+	},
+
+	currentState: null,
+
+	// Which events are allowed to go through in which state
+	allowedEvents: {
+		STATE_RECORD: ['click-event', 'input-event', 'nav-event', 'scroll-event'],
+		STATE_ADD_CHECK: ['click-event', 'scroll-event']
+	},
+
+	// All the events should go through this
+	emit: function(eventName, data) {
+		if (this.allowedEvents[this.currentState].indexOf(eventName) >= 0) {
+			ipcRenderer.sendToHost(eventName, data);
+		}
+	},
+
+	setState: function(state) {
+		if (this.states[state] !== undefined) {
+			this.currentState = state;
+		}
+	}
+};
+
+// Always start with the record state
+// Resetting after every page load should not matter since adding a check should
+// not span over page changes
+stateFilter.setState(stateFilter.states.STATE_RECORD);
 
 window.onload = () => {
 	console.log('listeners');
 
 	// Send click events and coordinates to the <webview> element
 	document.addEventListener('click', (e) => {
-		// If the click was a forward click from the recorder, let it through
-		if (unfinishedClick) {
-			unfinishedClick = false;
-		} else {
-			// Prevent click initially and decide in recorder whether to click or not
-			e.preventDefault();
+		handleScrolling();
+		handleInput();
 
-			handleScrolling();
-			handleInput();
-
-			let el = document.elementFromPoint(e.clientX, e.clientY);
-			ipcRenderer.sendToHost('click-event', { x: e.clientX, y: e.clientY, element: el });
-			unfinishedClick = true;
-		}
+		let el = document.elementFromPoint(e.clientX, e.clientY);
+		stateFilter.emit('click-event', { x: e.clientX, y: e.clientY, tagName: el.tagName });
 	});
 
 	document.addEventListener('keydown', (e) => {
@@ -62,7 +92,7 @@ window.onload = () => {
 			handleInput();
 
 			// Send new navigation
-			ipcRenderer.sendToHost('nav-event', { url: url });
+			stateFilter.emit('nav-event', { url: url });
 
 			// Submit the form
 			parentForm.submit();
@@ -102,23 +132,27 @@ window.onload = () => {
 	function handleInput() {
 		if (inputElement !== null) {
 			let center = getElementCenter(inputElement);
-			ipcRenderer.sendToHost('input-event', { x: center.x, y: center.y, input: inputElement.value });
+			stateFilter.emit('input-event', { x: center.x, y: center.y, input: inputElement.value });
 			inputElement = null;
 		}
 	}
 
 	function handleScrolling() {
 		if (window.scrollX !== lastScrolledX || window.scrollY !== lastScrolledY) {
-			ipcRenderer.sendToHost('scroll-event', { x: window.scrollX, y: window.scrollY });
+			stateFilter.emit('scroll-event', { x: window.scrollX, y: window.scrollY });
 			lastScrolledX = window.scrollX;
 			lastScrolledY = window.scrollY;
 		}
 	}
 
-	// Since click is prevented and sent to the recorder when recording,
-	// it must still be performed afterwards
-	ipcRenderer.on('click-forward', (e, coords) => {
-		document.elementFromPoint(coords.x, coords.y).click();
+	// A signal that the site needs to go to check adding state
+	ipcRenderer.on('add-check-state', () => {
+		stateFilter.setState(stateFilter.states.STATE_ADD_CHECK);
+	});
+
+	// A signal that the site needs to go to recording state
+	ipcRenderer.on('record-state', () => {
+		stateFilter.setState(stateFilter.states.STATE_RECORD);
 	});
 
 	// Actions playback
