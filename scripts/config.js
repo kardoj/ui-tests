7b651411b3ee99f15eec00f8361f8b32418a1e40
Kardo <kardoj@gmail.com>
2017-04-22 15:04:23 +0300

    Removes main process specific configuration away from renderer process config

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,5 @@
 let Config = {};
 ((ns) => {
-	ns.testLocation = 'tests';
-	ns.testFileExtension = '.uitest';
-
 	// Timeout between playback of the actions which do not trigger page load.
 	// Needs to be changed if the tested page contains animations that take longer
 	// than timeout milliseconds to complete. Should be able to be relatively short (200-500)
